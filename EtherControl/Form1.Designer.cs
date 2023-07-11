d5ead39f3decad4a0f6817f9c776d97145898076
XEDONO <palmas.yut@gmail.com>
2023-07-11 16:58:51 +0100

    Add project files.

---
+++ EtherControl/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EtherControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rjToggleButton1 = new CustomControls.RJControls.RJToggleButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rjToggleButton1
+            // 
+            this.rjToggleButton1.AutoSize = true;
+            this.rjToggleButton1.Location = new System.Drawing.Point(164, 16);
+            this.rjToggleButton1.MinimumSize = new System.Drawing.Size(60, 22);
+            this.rjToggleButton1.Name = "rjToggleButton1";
+            this.rjToggleButton1.OffBackColor = System.Drawing.Color.DarkRed;
+            this.rjToggleButton1.OffToggleColor = System.Drawing.Color.Black;
+            this.rjToggleButton1.OnBackColor = System.Drawing.Color.Chartreuse;
+            this.rjToggleButton1.OnToggleColor = System.Drawing.Color.WhiteSmoke;
+            this.rjToggleButton1.Size = new System.Drawing.Size(60, 22);
+            this.rjToggleButton1.TabIndex = 0;
+            this.rjToggleButton1.UseVisualStyleBackColor = true;
+            this.rjToggleButton1.CheckedChanged += new System.EventHandler(this.RjToggleButton1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(210, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(161, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(77, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Status: ";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.rjToggleButton1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 136);
+            this.panel1.TabIndex = 4;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(292, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 136);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CustomControls.RJControls.RJToggleButton rjToggleButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EtherControl/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EtherControl
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+     (
+         int nLeftRect,     // x-coordinate of upper-left corner
+         int nTopRect,      // y-coordinate of upper-left corner
+         int nRightRect,    // x-coordinate of lower-right corner
+         int nBottomRect,   // y-coordinate of lower-right corner
+         int nWidthEllipse, // width of ellipse
+         int nHeightEllipse // height of ellipse
+     );
+
+
+        private bool isAdapterEnabled;
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+        private readonly Timer timer1 = new Timer();
+        private bool drag = false;
+        private Point start_point = new Point(0, 0);
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            drag = true;
+            start_point = new Point(e.X, e.Y);
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - start_point.X, p.Y - start_point.Y);
+            }
+        }
+
+        private void Panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            isAdapterEnabled = IsEthernetAdapterEnabled();
+            rjToggleButton1.Checked = isAdapterEnabled;
+            UpdateLabel();
+            int monitorIndex = 1; // Index of the third monitor (zero-based)
+            Screen screen = Screen.AllScreens[monitorIndex];
+            int x = screen.WorkingArea.Left;
+            int y = screen.WorkingArea.Top;
+            Location = new Point(x, y);
+        }
+
+        private void UpdateSwitchAndLabel()
+        {
+            bool isAdapterEnabled = IsEthernetAdapterEnabled();
+            rjToggleButton1.Checked = isAdapterEnabled;
+            UpdateLabel();
+        }
+
+        private bool IsEthernetAdapterEnabled()
+        {
+            NetworkInterface[] adapters = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface adapter in adapters)
+            {
+                if (adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet &&
+                    adapter.OperationalStatus == OperationalStatus.Up)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void UpdateLabel()
+        {
+            label1.Text = rjToggleButton1.Checked ? "Enabled" : "Disabled";
+        }
+
+        private void EnableOrDisableEthernetAdapter(bool enable)
+        {
+            // You can implement code to enable or disable the Ethernet adapter here.
+            // Keep in mind that enabling/disabling network interfaces typically requires
+            // administrative privileges, so you need to handle that accordingly.
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            // Apply the changes when the Apply button is clicked
+            if (rjToggleButton1.Checked == true)
+            {
+                try
+                {
+
+                    Process prfo = Process.Start(@"enable.bat");
+                    int id = prfo.Id;
+                    Process temp = Process.GetProcessById(id);
+                    Visible = true;
+                    temp.WaitForExit();
+                    Visible = true;
+                    UpdateLabel();
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("NOT ENABLED", "Error");
+                }
+            }
+        }
+
+        private void RjToggleButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rjToggleButton1.Checked == false)
+            {
+
+                try
+                {
+
+                    Process prfo = Process.Start(@"disable.bat");
+                    int id = prfo.Id;
+                    Process temp = Process.GetProcessById(id);
+                    Visible = false;
+                    temp.WaitForExit();
+                    Visible = true;
+                    UpdateLabel();
+
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("NOT DISABLED", "Error");
+                }
+            }
+
+            
+
+
+             
+
+            
+
+        }
+       
+    }
+}
